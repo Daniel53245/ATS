3a9b1faef1744d2b7c4bb5d0d3708de6abe86025
Boyu Dai <daniel53245@outlook.com>
2025-06-04 14:14:06 +1000

    create hash fucntions

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -1,12 +1,12 @@
-from sqlalchemy import Column, Interger, String,DateTime, null, true
+from sqlalchemy import Column, Integer, String, DateTime, null, true
 from sqlalchemy.sql import func
-from app.database import Base 
+from app.database import Base
+
 
 class User(Base):
-        __tablename__ = "users"
-        
-        id = Column(Interger,primary_key=True,index=True)
-        email = Column(String,unique=True,index=True,nullable=False)
-        hashed_password = Column(String,nullable=False)
-        created_at = Column(DateTime(timezone=True),server_default=func.now())
-        
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
